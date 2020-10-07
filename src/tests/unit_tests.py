85832c35ed14a7c83bfc0d4f53399f0eb6ce9f65
Marc Elrick <marc@launchhubl.com>
2020-10-07 14:35:32 +0100

    Reformatted code ahead of pipeline test.

--- src/tests/unit_tests.py
+++ src/tests/unit_tests.py
@@ -1,9 +1,12 @@
 import unittest
+
+
 # Smoke test to verify test suite works
 class SmokeTest(unittest.TestCase):
 
     def test_smoke(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
 
 if __name__ == '__main__':
     unittest.main()
